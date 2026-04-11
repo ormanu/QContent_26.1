3bd3845876c61e6395f94c523c6b0cfe71e0af57
ormanu <mateiorman50@gmail.com>
2026-04-11 21:10:10 +0300

    particle reg

--- src/main/java/ormanu/qcontent/QContent.java
+++ src/main/java/ormanu/qcontent/QContent.java
@@ -2,6 +2,11 @@ package ormanu.qcontent;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
+import net.minecraft.core.Registry;
+import net.minecraft.core.particles.SimpleParticleType;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ormanu.qcontent.config.QConfig;
@@ -11,12 +16,16 @@ public class QContent implements ModInitializer {
 	public static final String MOD_ID = "qcontent";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	public static final SimpleParticleType SCULK_SWEEP = FabricParticleTypes.simple();
+
 	@Override
 	public void onInitialize() {
 
 		eu.midnightdust.lib.config.MidnightConfig.init("qcontent", QConfig.class);
 		ModItems.initialize();
 		LOGGER.info("QContent Main Class Init");
+
+		Registry.register(BuiltInRegistries.PARTICLE_TYPE, Identifier.fromNamespaceAndPath(MOD_ID, "sculk_sweep"), SCULK_SWEEP);
 	}
 
 }
